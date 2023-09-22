c7a7184fd03082e0d014e8569fc21a7bda81ae5d
Dexter Leng <dexterlengcs@gmail.com>
2023-09-22 19:39:14 +0800

    fix opacity

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default async function Index() {
         </div>
       </nav>
 
-      <div className="animate-in flex flex-col gap-14 opacity-0 max-w-4xl px-3 py-16 lg:py-24 text-foreground">
+      <div className="animate-in flex flex-col gap-14 max-w-4xl px-3 py-16 lg:py-24 text-foreground">
         <div className="flex flex-col items-center mb-4 lg:mb-12">
           <div className="flex gap-8 justify-center items-center">
             <Link href="https://supabase.com/" target="_blank">
