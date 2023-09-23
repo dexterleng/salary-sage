cd30828a98c54ee1d18491583889501410c1ce3b
Dexter Leng <dexterlengcs@gmail.com>
2023-09-23 17:09:48 +0800

    Fix build (hopefully)

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,6 +3,8 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { redirect } from "next/navigation"
 import { cookies } from 'next/headers'
 
+export const dynamic = 'force-dynamic'
+
 export default async function DashboardLayout({
   children,
 }: {
