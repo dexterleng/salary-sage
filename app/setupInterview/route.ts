d8b9d1ed2a61c0ac1b62c5235362497b5da90c3e
Quan Teng Foong <e0564970@u.nus.edu>
2023-09-24 17:41:15 +0800

    add setupInterview route

---
+++ app/setupInterview/route.ts
@@ -0,0 +1,38 @@
+import { createClient } from '@supabase/supabase-js'
+import { NextResponse } from 'next/server'
+
+export const dynamic = 'force-dynamic'
+
+/**
+ * Creates a new entry in the interviewData table and returns the interviewId.
+ * 
+ * Expect POST request with JSON body:
+ * {
+ *   "interviewData": {
+ *     "companyName": str,
+ *     "role": str,
+ *     "minExpectedComp": int
+ *     "maxExpectedComp": int
+ *   }
+ * } 
+ */
+export async function POST(request: Request) {
+  try {
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+
+    const { interview } = await request.json();
+
+    const { data, statusText } = await supabase
+      .from('interview')
+      .insert(interview)
+      .select('id');
+
+    const interviewId = data![0].id;
+    return NextResponse.json({ interviewId }, { status: 200 })
+  } catch (error: any) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
