626fc954adff742e47872c0f02fe5d368aa29da9
Charisma Kausar <charisma.kausar@gmail.com>
2023-09-25 03:48:43 +0800

    refactor: Remove repetitive code in scores-circular component

--- components/feedback/scores-circular.tsx
+++ components/feedback/scores-circular.tsx
@@ -9,77 +9,43 @@ type ScoresCircularProps = {
 
 export default function ScoresCircular({ clarityScore, confidenceScore }: ScoresCircularProps) {
   const overallScore = Math.round((clarityScore + confidenceScore) / 2);
+
+  let metrics = [
+    { score: clarityScore, title: "Clarity" },
+    { score: confidenceScore, title: "Confidence" },
+    { score: overallScore, title: "Overall" }
+  ];
+
   return (
     <div className="flex gap-12">
-      <div>
-        <div style={{ width: 126, height: 126 }}>
-          <CircularProgressbar
-            value={clarityScore}
-            text={`${clarityScore}%`}
-            styles={buildStyles({
-              rotation: 0,
-              strokeLinecap: 'round',
-              textSize: '16px',
-              pathTransitionDuration: 0.5,
-              pathColor: '#007F5F',
-              textColor: '#06110D',
-              trailColor: '#F1F8F6',
-              backgroundColor: '#F1F8F6',
-            })
-            }
-            background={true}
-          />
-        </div>
-        <TypographyBody className="mt-2 text-center">
-          Clarity
-        </TypographyBody>
-      </div>
-      <div>
-        <div style={{ width: 126, height: 126 }}>
-          <CircularProgressbar
-            value={confidenceScore}
-            text={`${confidenceScore}%`}
-            styles={buildStyles({
-              rotation: 0,
-              strokeLinecap: 'round',
-              textSize: '16px',
-              pathTransitionDuration: 0.5,
-              pathColor: '#007F5F',
-              textColor: '#06110D',
-              trailColor: '#F1F8F6',
-              backgroundColor: '#F1F8F6',
-            })
-            }
-            background={true}
-          />
-        </div>
-        <TypographyBody className="mt-2 text-center">
-          Confidence
-        </TypographyBody>
-      </div>
-      <div>
-        <div style={{ width: 126, height: 126 }}>
-          <CircularProgressbar
-            value={overallScore}
-            text={`${overallScore}%`}
-            styles={buildStyles({
-              rotation: 0,
-              strokeLinecap: 'round',
-              textSize: '16px',
-              pathTransitionDuration: 0.5,
-              pathColor: '#007F5F',
-              textColor: '#06110D',
-              trailColor: '#F1F8F6',
-              backgroundColor: '#F1F8F6',
-            })
-            }
-            background={true}
-          />
-        </div>
-        <TypographyBody className="mt-2 text-center">
-          Overall
-        </TypographyBody>
-      </div>
+      {metrics.map(
+        metric =>
+        (
+          <div>
+            <div style={{ width: 126, height: 126 }}>
+              <CircularProgressbar
+                value={metric.score}
+                text={`${metric.score}%`}
+                styles={buildStyles({
+                  rotation: 0,
+                  strokeLinecap: 'round',
+                  textSize: '16px',
+                  pathTransitionDuration: 0.5,
+                  pathColor: '#007F5F',
+                  textColor: '#06110D',
+                  trailColor: '#F1F8F6',
+                  backgroundColor: '#F1F8F6',
+                })
+                }
+                background={true}
+              />
+            </div>
+            <TypographyBody className="mt-2 text-center">
+              {metric.title}
+            </TypographyBody>
+          </div>
+        )
+      )}
     </div>
   );
 }
