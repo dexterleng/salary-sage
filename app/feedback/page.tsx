dfcb146e7c33b1ed07e33db34d4dc9383e417eba
Charisma Kausar <charisma.kausar@gmail.com>
2023-09-25 03:39:01 +0800

    feat: Add positive and negative feedback card components

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { TypographyH1, TypographyH2 } from "@/components/ui/typography";
+import { TypographyBody, TypographyH1, TypographyH2, TypographyLarge } from "@/components/ui/typography";
 import ScoresCircular from "@/components/feedback/scores-circular";
 import MockNegotiation from "@/components/feedback/mock-nego";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from "@/components/ui/accordion";
+import { CheckCircledIcon, CrossCircledIcon } from "@radix-ui/react-icons";
 
 export default function Feedback() {
   const clarity = 78;
@@ -13,9 +15,9 @@ export default function Feedback() {
   const negativeFeedback = ["You were not clear in your speech", "You were not confident in your speech"];
 
   return (
-    <div className="px-32 py-12">
-      <TypographyH1>Your Feedback</TypographyH1>
-      <div className="flex py-6 gap-12">
+    <div className="px-32 py-12 justify-center flex flex-col items-center">
+      <TypographyH1 className="w-full">Your Feedback</TypographyH1>
+      <div className="flex py-6 gap-12 flex-wrap">
         <div className="flex flex-col gap-12">
           <Card>
             <CardHeader>
@@ -24,7 +26,7 @@ export default function Feedback() {
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <div className="px-6 pb-6">
+              <div className="px-12 pb-6">
                 <ScoresCircular clarityScore={clarity} confidenceScore={confidence} />
               </div>
             </CardContent>
@@ -34,15 +36,54 @@ export default function Feedback() {
               <CardTitle>
                 <TypographyH2 className="ml-2">Where you did well</TypographyH2>
               </CardTitle>
-              <CardContent>
-                <div className="px-6 pb-6">
-                  
-                </div>
-              </CardContent>
             </CardHeader>
             <CardContent>
-              <div className="px-6 pb-6">
-                
+              <div className="pb-6 px-2">
+                {positiveFeedback.map(line =>
+                  <Accordion type="single" collapsible>
+                    <AccordionItem value="item-1">
+                      <AccordionTrigger>
+                        <div className="flex gap-2">
+                          <CheckCircledIcon className="h-6 w-6 stroke-primary" />
+                          <TypographyLarge>Title</TypographyLarge>
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        <TypographyBody>
+                          {line}
+                        </TypographyBody>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                )}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <TypographyH2 className="ml-2">Where you can improve</TypographyH2>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="pb-6 px-2">
+                {negativeFeedback.map(line =>
+                  <Accordion type="single" collapsible>
+                    <AccordionItem value="item-1">
+                      <AccordionTrigger>
+                        <div className="flex gap-2">
+                          <CrossCircledIcon className="h-6 w-6 stroke-destructive" />
+                          <TypographyLarge>Title</TypographyLarge>
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        <TypographyBody>
+                          {line}
+                        </TypographyBody>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                )}
               </div>
             </CardContent>
           </Card>
@@ -55,7 +96,7 @@ export default function Feedback() {
           </CardHeader>
           <CardContent>
             <div className="px-2 pb-6">
-              <MockNegotiation position="Software Engineer" company="Open Government Products" hintCount={3} interviewerStyles={["Arrogant", "Difficult"]} transcript={["Hi", "Hello"]}/>
+              <MockNegotiation position="Software Engineer" company="Open Government Products" hintCount={3} interviewerStyles={["Arrogant", "Difficult"]} transcript={["Hi", "Hello"]} />
             </div>
           </CardContent>
         </Card>
