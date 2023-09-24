ad7a593cb710aee036f28a8ce6ece2de7f5325ec
Quan Teng Foong <e0564970@u.nus.edu>
2023-09-24 21:50:18 +0800

    add get and store interviewTranscript

--- app/startInterview/route.ts
+++ app/startInterview/route.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextResponse } from 'next/server';
 
 const openai = new OpenAI({
   // requires OPENAI_API_KEY to be set in .env
@@ -18,33 +19,44 @@ export const runtime = 'edge';
  * }
  */
 export async function POST(req: Request) {
+  try {
+    // Get interview data from Supabase
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+    const { interviewId } = await req.json();
+    const { data } = await supabase
+      .from('interview')
+      .select('*')
+      .eq('id', interviewId);
+    const { companyName, role, minExpectedComp, maxExpectedComp } = data![0];
 
-  // Get interview data from Supabase
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-  const { interviewId } = await req.json();
-  const { data } = await supabase
-    .from('interview')
-    .select('*')
-    .eq('id', interviewId);
-  const { companyName, role, minExpectedComp, maxExpectedComp } = data![0];
-
-  // Send prompt to OpenAI
-  // TODO: insert real prompt here
-  const prompt = `You are a negotiator. You are negotiating with a candidate for \
+    // Send prompt to OpenAI
+    // TODO: insert real prompt here
+    const prompt = `You are a negotiator. You are negotiating with a candidate for \
 a ${role} position at ${companyName}. The candidate's expected total compensation \
 is between $${minExpectedComp} and $${maxExpectedComp}. You will have a conversation \
 with the candidate to determine the candidate's expected total compensation. \
 Please ask your first question.`;
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: [
-      {"role": "system", "content": prompt}
-    ]
-  });
-  const stream = OpenAIStream(response);
-  return new StreamingTextResponse(stream);
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages: [
+        {"role": "system", "content": prompt}
+      ]
+    });
+    const stream = OpenAIStream(response);
+
+    // Store prompt into Supabase
+    const { error } = await supabase
+      .from('interview_transcript')
+      .insert({ id: interviewId, transcript: prompt });
+
+    // console.log('error', error);
+
+    return new StreamingTextResponse(stream);
+  } catch (error: any) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
