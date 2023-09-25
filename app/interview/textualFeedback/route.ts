34abe92d47739d4f4efe284e0aba6a1945c96c14
Quan Teng Foong <e0564970@u.nus.edu>
2023-09-25 18:48:07 +0800

    add intervew/feedback and textualFeedback routes

--- app/interview/textualFeedback/route.ts
+++ app/interview/textualFeedback/route.ts
@@ -11,17 +11,14 @@ const openai = new OpenAI({
 export const runtime = 'edge';
 
 /**
- * Given an interviewId, gives feedback to the user in the below format.
+ * Given an interviewId, gives feedback to the user in the form of text.
  * 
  * Expects GET request with url parameter: interviewId
  * 
  * Returns JSON response:
  * {
  *   "interviewId": int,
- *   "confidence": int,
- *   "communication": int,
- *   "goodPoints": [str],
- *   "badPoints": [str]
+ *   "feedback": str
  * }
  * 
  */
@@ -42,34 +39,46 @@ export async function GET(req: Request) {
       .select('isInterviewer, transcript')
       .eq('id', interviewId)
       .order('created_at', { ascending: true });
+    
+    // Get interview feedback from Supabase
+    const { data: feedbackData, error: feedbackFetchError } = await supabase
+      .from('interview_feedback')
+      .select('confidence, communication, goodPoints, badPoints')
+      .eq('id', interviewId);
+
+    if (feedbackData?.length === 0) {
+      // Send a get request to interview/feedback route
+      // FIXME: doesn't work
+      /*
+      console.log("fetching");
+      fetch('/interview/feedback?interviewId=' + interviewId);
+      console.log("fetched");
+      // Get data again from supabase
+      const { data: feedbackData, error: feedbackFetchError } = await supabase
+        .from('interview_feedback')
+        .select('confidence, communication, goodPoints, badPoints')
+        .eq('id', interviewId);
+        */
+      console.log("no feedback found");
+    }
 
     // Send prompt to OpenAI
     // TODO: insert real prompt here
-    const prompt = `You have been provided with the transcript of a negotiation. Provide feedback to the
-    candidate on their negotiation skills. Your response should be of the format:
-    {
-      "confidence": int,
-      "communication": int,
-      "goodPoints": [str],
-      "badPoints": [str]
-    }`;
+    const prompt = `You have been provided with the transcript of a negotiation, along with
+    feedback from the interviewer. Provide feedback to the candidate on their negotiation skills.
+    Your response should be in the form of text.`;
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
+      stream: true,
       messages: [
         {"role": "system", "content": JSON.stringify(transcriptData)},
+        {"role": "system", "content": JSON.stringify(feedbackData)},
         {"role": "system", "content": prompt}
       ]
     });
+    const stream = OpenAIStream(response);
 
-    const contentAsJson = JSON.parse(response.choices[0].message.content ?? '');
-    const { confidence, communication, goodPoints, badPoints } = contentAsJson;
-
-    const { error: feedbackInsertError } = await supabase
-      .from('interview_feedback')
-      .insert({ id: interviewId, confidence: confidence, communication: communication,
-         goodPoints: goodPoints, badPoints: badPoints});
-
-    return NextResponse.json(contentAsJson, { status: 200 });
+    return new StreamingTextResponse(stream);
   } catch (error: any) {
     return NextResponse.json({ error }, { status: 500 });
   }
