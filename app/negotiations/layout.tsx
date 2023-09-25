1c3c99e97efaf616d087ea63299403a3c080060e
Dexter Leng <dexterlengcs@gmail.com>
2023-09-25 16:25:54 +0800

    Negotiations: redirect to login if unauthenticated

---
+++ app/negotiations/layout.tsx
@@ -0,0 +1,30 @@
+import { NavigationBar } from "@/components/NavigationBar"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation"
+import { cookies } from 'next/headers'
+import { Metadata } from "next";
+
+export const dynamic = 'force-dynamic'
+
+export const metadata: Metadata = {
+  title: "Negotiations",
+};
+
+export default async function NegotiationsLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const supabase = createServerComponentClient({ cookies })
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect('/login/')
+  }
+
+  return (
+    <>{children}</>
+  )
+}
