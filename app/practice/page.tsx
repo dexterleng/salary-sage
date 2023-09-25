3ced8efd28af50fe478781b9298488356cc4430d
Charisma Kausar <charisma.kausar@gmail.com>
2023-09-26 04:45:27 +0800

    feat: Create alert dialog to confirm practice start

--- app/practice/page.tsx
+++ app/practice/page.tsx
@@ -6,13 +6,35 @@ import Link from "next/link";
 import { BotIcon, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import AudioRecorder from "@/components/practice/audio-recorder";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+
 
 export default function Practice() {
   const [hasPracticeStarted, setHasPracticeStarted] = useState(false);
   const [isInterviewerSpeaking, setIsInterviewerSpeaking] = useState(false);
   const [isProcessing, setIsProcessing] = useState(false);
   const [responseData, setResponseData] = useState<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    if (hasPracticeStarted) {
+      setIsInterviewerSpeaking(true);
+      setTimeout(() => {
+        const audioElement = document.getElementById('interviewer-audio') as HTMLAudioElement;
+        if (audioElement) {
+          audioElement.play();
+        }
+      }, 1000);
+    }
+  }, [hasPracticeStarted]);
 
   // temp delay function to simulate API request
   const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
@@ -31,10 +53,10 @@ export default function Practice() {
       // const data = await response.json();
       // setResponseData(data);
 
-      let audioUrl = require('@/public/audio/sample-audio.mp3');
-      let sound = new Audio(audioUrl);
-      setResponseData(sound);
-      sound.play();
+      // let audioUrl = require('@/public/audio/abstract.mp3');
+      // let sound = new Audio(audioUrl);
+      // setResponseData(sound);
+      // sound.play();
     } catch (error) {
       console.error('Error uploading audio:', error);
     }
@@ -69,17 +91,18 @@ export default function Practice() {
                     {
                       isProcessing
                         ? 'Waiting for your interviewer to reply...'
-                        : <audio controls autoPlay={hasPracticeStarted} 
-                        onPlay={() => { setHasPracticeStarted(true); setIsInterviewerSpeaking(true); }} 
-                        onEnded={() => setIsInterviewerSpeaking(false)}
-                        src='/audio/abstract.mp3'></audio>
+                        : <audio controls autoPlay={hasPracticeStarted}
+                          id="interviewer-audio"
+                          onPlay={() => { setIsInterviewerSpeaking(true); }}
+                          onEnded={() => setIsInterviewerSpeaking(false)}
+                          src='/audio/abstract.mp3'></audio>
                     }
                   </div>
                 </div>
               </div>
             </CardContent>
           </Card>
-          <Card className={`flex-1 ${isProcessing || isInterviewerSpeaking || !hasPracticeStarted ? 'opacity-50 bg-muted/10' : 'opacity-100 bg-card'}`}>
+          <Card className={`flex-1 ${isProcessing || isInterviewerSpeaking ? 'opacity-50 bg-muted/10' : 'opacity-100 bg-card'}`}>
             <CardHeader>
               <CardTitle>
                 <div className="flex items-center">
@@ -94,7 +117,7 @@ export default function Practice() {
             </CardHeader>
             <CardContent className="relative flex flex-col items-center justify-center h-[calc(50vh-80px)]">
               <div className="px-2 pb-6">
-                <AudioRecorder onSubmit={handleApiRequest} isProcessing={isProcessing || isInterviewerSpeaking || !hasPracticeStarted} />
+                <AudioRecorder onSubmit={handleApiRequest} isProcessing={isProcessing || isInterviewerSpeaking} />
               </div>
               <TypographySubtle className="absolute right-4 bottom-0">
                 Stuck? <Button variant="link" className="text-primary hover:underline -ml-4">Get Hints</Button>
@@ -103,6 +126,19 @@ export default function Practice() {
           </Card>
         </div>
       </div>
+      <AlertDialog open={!hasPracticeStarted}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Mock Salary Negotiation with AI</AlertDialogTitle>
+            <AlertDialogDescription>
+              This is a salary negotiation practice session with an AI interviewer.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction onClick={() => setHasPracticeStarted(true)}>Start Practice</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
