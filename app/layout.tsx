3281ee63d47d283ddb05246b15ee346a0a1b6250
Dexter Leng <dexterlengcs@gmail.com>
2023-09-25 16:42:44 +0800

    Fix build 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,8 @@ export const metadata = {
   description: 'Practice your salary negotiation skills with AI',
 }
 
+export const dynamic = 'force-dynamic'
+
 export default async function RootLayout({
   children,
 }: {
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
 import { TypographyH1, TypographyH2 } from '@/components/ui/typography'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export const dynamic = 'force-dynamic'
-
 export default async function Index() {
-  const supabase = createServerComponentClient({ cookies })
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
   return (
     <div className="w-full flex flex-col items-center bg-[url('/images/hero-bg.png')]">
       <div className="flex h-[calc(100vh-55px)] justify-center gap-8">
