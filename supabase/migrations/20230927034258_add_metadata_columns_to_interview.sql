bd8a50267472761e4f3bb7cf5b0ac22842abb166
sherrpass <sherwinpoh3@gmail.com>
2023-09-27 19:43:21 +0800

    Add interview setup logic in post negotiations and placeholder values

--- supabase/migrations/20230927034258_add_metadata_columns_to_interview.sql
+++ supabase/migrations/20230927034258_add_metadata_columns_to_interview.sql
@@ -1,10 +1,6 @@
 ALTER TABLE "public"."interview"
-ADD COLUMN company TEXT,
 ADD COLUMN job_title TEXT,
 ADD COLUMN market_analysis TEXT,
 ADD COLUMN hm_guidelines TEXT,
 ADD COLUMN meta_instructions TEXT,
-ADD COLUMN suitability_analysis TEXT,
-ADD COLUMN is_setup_complete BOOLEAN NOT NULL DEFAULT FALSE;
-
-
+ADD COLUMN suitability_analysis TEXT
