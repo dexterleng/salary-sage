e3a0bc18c6feae9073d5324f1442c1094a725b10
Charisma Kausar <charisma.kausar@gmail.com>
2023-09-29 04:31:29 +0800

    fix: Make feedback page responsive

--- app/negotiations/[id]/feedback/page.tsx
+++ app/negotiations/[id]/feedback/page.tsx
@@ -106,153 +106,155 @@ export default function Feedback({ params }: { params: { id: string } }) {
   }, [isLoading]);
 
   return (
-    <div className="px-32 py-12 justify-center flex flex-col items-center">
-      <div className="flex w-full justify-between items-center">
-        <TypographyH1 className="w-fit">Your Feedback</TypographyH1>
-        <Link href="/dashboard"><Button className="w-fit" size="lg">Return to Dashboard</Button></Link>
-      </div>
-      <div className="my-4 ml-4 w-full flex justify-center relative">
-        <p
-          className={`w-full text-md text-slate-600 transition-opacity duration-1000 ${opacityClass} absolute`}
-        >
-          {isLoading ? loadingTexts[loadingTextIndex] : ""}
-        </p>
-      </div>
-      <div className="flex py-6 gap-12 flex-wrap w-full">
-        <div className="flex flex-1 grow flex-col gap-12">
-          <Card>
-            <CardHeader>
-              <CardTitle>
-                <TypographyH2 className="ml-2">Scores</TypographyH2>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="px-6 pb-6">
-                {quantitativeFeedbacks?.assertiveness
-                  ? <ScoresCircular metrics={quantitativeFeedbacks} isEvaluationShown={true} />
-                  : <div>
-                    <Skeleton
-                      count={5}
-                      circle
-                      height="108px"
-                      width="108px"
-                      containerClassName="avatar-skeleton flex gap-4"
-                      className="block flex-1"
-                    />
-                    <Skeleton
-                      count={5}
-                      containerClassName="skeleton flex gap-4"
+    <div className="px-4 py-12 justify-center flex flex-col items-center">
+      <div className="max-w-7xl">
+        <div className="flex w-full justify-between items-center">
+          <TypographyH1 className="w-fit">Your Feedback</TypographyH1>
+          <Link href="/dashboard"><Button className="w-fit" size="lg">Return to Dashboard</Button></Link>
+        </div>
+        <div className="my-4 ml-4 w-full flex justify-center relative">
+          <p
+            className={`w-full text-md text-slate-600 transition-opacity duration-1000 ${opacityClass} absolute`}
+          >
+            {isLoading ? loadingTexts[loadingTextIndex] : ""}
+          </p>
+        </div>
+        <div className="flex py-6 gap-12 flex-wrap w-full">
+          <div className="flex flex-1 grow flex-col gap-12">
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <TypographyH2 className="ml-2">Scores</TypographyH2>
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="px-6 pb-6">
+                  {quantitativeFeedbacks?.assertiveness
+                    ? <ScoresCircular metrics={quantitativeFeedbacks} isEvaluationShown={true} />
+                    : <div>
+                      <Skeleton
+                        count={5}
+                        circle
+                        height="108px"
+                        width="108px"
+                        containerClassName="avatar-skeleton flex gap-4"
+                        className="block flex-1"
+                      />
+                      <Skeleton
+                        count={5}
+                        containerClassName="skeleton flex gap-4"
+                        className="block flex-1 pt-2 mt-2"
+                      />
+                    </div>
+                  }
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <TypographyH2 className="ml-2">Where you did well</TypographyH2>
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="pb-6 px-2">
+                  {positiveFeedbacks?.length > 0
+                    ? positiveFeedbacks.map(feedback =>
+                      <Accordion type="single" collapsible key={feedback.evaluation}>
+                        <AccordionItem value="item-1">
+                          <AccordionTrigger>
+                            <div className="flex gap-2 text-left">
+                              <CheckCircle2 className="h-6 w-6 stroke-primary" />
+                              <TypographyLarge>{feedback.title}</TypographyLarge>
+                            </div>
+                          </AccordionTrigger>
+                          <AccordionContent>
+                            <TypographyBody>
+                              {feedback.evaluation}
+                            </TypographyBody>
+                            <TypographySubtle className="mt-2">
+                              <i>Refer:</i>
+                              <Button variant="link" className="-ml-3 text-left" onClick={() => setSearchedCitation(feedback.citation)}>
+                                <p className="line-clamp-2">"{feedback.citation}"</p>
+                              </Button>
+                            </TypographySubtle>
+                          </AccordionContent>
+                        </AccordionItem>
+                      </Accordion>
+                    )
+                    : <Skeleton
+                      count={3}
+                      containerClassName="skeleton flex flex-col gap-4"
                       className="block flex-1 pt-2 mt-2"
                     />
-                  </div>
-                }
-              </div>
-            </CardContent>
-          </Card>
-          <Card>
+                  }
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <TypographyH2 className="ml-2">Where you can improve</TypographyH2>
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="pb-6 px-2">
+                  {negativeFeedbacks?.length > 0
+                    ? negativeFeedbacks.map(feedback =>
+                      <Accordion type="single" collapsible key={feedback.evaluation}>
+                        <AccordionItem value="item-1">
+                          <AccordionTrigger>
+                            <div className="flex gap-2 text-left">
+                              <XCircle className="h-6 w-6 stroke-destructive" />
+                              <TypographyLarge>{feedback.title}</TypographyLarge>
+                            </div>
+                          </AccordionTrigger>
+                          <AccordionContent>
+                            <TypographyBody>
+                              {feedback.evaluation}
+                            </TypographyBody>
+                            <TypographySubtle className="mt-2">
+                              <i>Refer:</i>
+                              <Button variant="link" className="-ml-3 text-left" onClick={() => setSearchedCitation(feedback.citation)}>
+                                <p className="line-clamp-2">"{feedback.citation}"</p>
+                              </Button>
+                              <br />
+                              <br />
+                              <i>Suggestion:</i>
+                              <br />
+                              <p>"{feedback.suggestion}"</p>
+                            </TypographySubtle>
+                          </AccordionContent>
+                        </AccordionItem>
+                      </Accordion>
+                    )
+                    : <Skeleton
+                      count={3}
+                      containerClassName="skeleton flex flex-col gap-4"
+                      className="block flex-1 pt-2 mt-2"
+                    />
+                  }
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+          <Card className="flex-1">
             <CardHeader>
               <CardTitle>
-                <TypographyH2 className="ml-2">Where you did well</TypographyH2>
+                <TypographyH2 className="ml-2">Transcript</TypographyH2>
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <div className="pb-6 px-2">
-                {positiveFeedbacks?.length > 0
-                  ? positiveFeedbacks.map(feedback =>
-                    <Accordion type="single" collapsible key={feedback.evaluation}>
-                      <AccordionItem value="item-1">
-                        <AccordionTrigger>
-                          <div className="flex gap-2 text-left">
-                            <CheckCircle2 className="h-6 w-6 stroke-primary" />
-                            <TypographyLarge>{feedback.title}</TypographyLarge>
-                          </div>
-                        </AccordionTrigger>
-                        <AccordionContent>
-                          <TypographyBody>
-                            {feedback.evaluation}
-                          </TypographyBody>
-                          <TypographySubtle className="mt-2">
-                            <i>Refer:</i>
-                            <Button variant="link" className="-ml-3 text-left" onClick={() => setSearchedCitation(feedback.citation)}>
-                              <p className="line-clamp-2">"{feedback.citation}"</p>
-                            </Button>
-                          </TypographySubtle>
-                        </AccordionContent>
-                      </AccordionItem>
-                    </Accordion>
-                  )
-                  : <Skeleton
-                    count={3}
-                    containerClassName="skeleton flex flex-col gap-4"
-                    className="block flex-1 pt-2 mt-2"
-                  />
-                }
-              </div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>
-                <TypographyH2 className="ml-2">Where you can improve</TypographyH2>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="pb-6 px-2">
-                {negativeFeedbacks?.length > 0
-                  ? negativeFeedbacks.map(feedback =>
-                    <Accordion type="single" collapsible key={feedback.evaluation}>
-                      <AccordionItem value="item-1">
-                        <AccordionTrigger>
-                          <div className="flex gap-2 text-left">
-                            <XCircle className="h-6 w-6 stroke-destructive" />
-                            <TypographyLarge>{feedback.title}</TypographyLarge>
-                          </div>
-                        </AccordionTrigger>
-                        <AccordionContent>
-                          <TypographyBody>
-                            {feedback.evaluation}
-                          </TypographyBody>
-                          <TypographySubtle className="mt-2">
-                            <i>Refer:</i>
-                            <Button variant="link" className="-ml-3 text-left" onClick={() => setSearchedCitation(feedback.citation)}>
-                              <p className="line-clamp-2">"{feedback.citation}"</p>
-                            </Button>
-                            <br />
-                            <br />
-                            <i>Suggestion:</i>
-                            <br />
-                            <p>"{feedback.suggestion}"</p>
-                          </TypographySubtle>
-                        </AccordionContent>
-                      </AccordionItem>
-                    </Accordion>
-                  )
-                  : <Skeleton
-                    count={3}
-                    containerClassName="skeleton flex flex-col gap-4"
-                    className="block flex-1 pt-2 mt-2"
-                  />
-                }
+              <div className="px-2 pb-6">
+                <Transcript transcript={transcript} citation={searchedCitation} />
               </div>
             </CardContent>
           </Card>
         </div>
-        <Card className="flex-1">
-          <CardHeader>
-            <CardTitle>
-              <TypographyH2 className="ml-2">Transcript</TypographyH2>
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="px-2 pb-6">
-              <Transcript transcript={transcript} citation={searchedCitation} />
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-      <div className="flex gap-4 mt-12">
-        <Link href="/dashboard"><Button className="w-fit" size="lg">Return to Dashboard</Button></Link>
-        <Link href="/dashboard"><Button className="w-fit" variant="secondary" size="lg">Practice Again</Button></Link>
+        <div className="flex gap-4 mt-12">
+          <Link href="/dashboard"><Button className="w-fit" size="lg">Return to Dashboard</Button></Link>
+          <Link href="/dashboard"><Button className="w-fit" variant="secondary" size="lg">Practice Again</Button></Link>
+        </div>
       </div>
     </div>
   );
